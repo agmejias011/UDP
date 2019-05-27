fc8b4bfe1041a17a7211e679aa8dc6893e22b1c1
agmejias011 <agmejias011@hotmail.com>
2019-05-26 22:56:05 -0400

    Update TCPClient.py

--- TCP/TCPClient.py
+++ TCP/TCPClient.py
@@ -1,12 +1,37 @@
-#TCPClient.py
+#source: https://pymotw.com
+import socket
+import sys
 
-from socket import socket, AF_INET, SOCK_STREAM
-serverName = 'localhost'
-serverPort = 12001
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName, serverPort))
-message = raw_input('Input lowercase sentence: ')
-clientSocket.send(message)
-modifiedMessage = clientSocket.recv(2048)
-print 'From Server: ', modifiedMessage
-clientSocket.close()
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('localhost', 12001)
+print >>sys.stderr, 'connecting to %s port %s' % server_address
+print("Type ""CTRL+C"" to finish")
+
+#connecting to server
+sock.connect(server_address)
+
+#getting the username of the user
+username = raw_input("Username: ");
+
+while True:
+    try:
+        # Send data
+        message = raw_input("{}: ".format(username))
+        sock.sendall(message)
+
+        # Look for the response
+        amount_received = 0
+        amount_expected = len(message)
+    
+        #checking if any message was sent
+        while amount_received < amount_expected:
+            data = sock.recv(2048)
+            amount_received += len(data)
+            print >>sys.stderr, 'received "%s"' % data
+    except KeyboardInterrupt:
+        print ("Interrupted by CTRL+C")        
+
+sock.close()
