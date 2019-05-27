9c1737701d9b06c925168af863d5b32e34706506
agmejias011 <agmejias011@hotmail.com>
2019-05-26 22:54:49 -0400

    Update UDPClient.py

--- UDP/UDPClient.py
+++ UDP/UDPClient.py
@@ -6,9 +6,24 @@ serverName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = raw_input('Input lowercase sentence: ')
+clientSocket.settimeout(1)
+
+
+#send data
 clientSocket.sendto(message, (serverName, serverPort))
-modifiedMessage, addr = clientSocket.recvfrom(2048)
-print modifiedMessage, addr
+
+try:
+    #receive response
+    modifiedMessage, addr = clientSocket.recvfrom(2048)
+    print (modifiedMessage, addr)
+except:
+    print ("REQUEST TIMED OUT")
+    #Allow the client to give up if no response has been reveived within 1 second.
+    inputTimeOut = raw_input("Do you want to continue, or exit. Type any value to exit\n")        
+    #exit
+    if inputTimeOut == "[e]":
+        clientSocket.close()
+
 clientSocket.close()
 
-#Allow the client to give up if no response has been reveived within 1 second.
+
